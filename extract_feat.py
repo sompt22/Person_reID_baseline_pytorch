fc49f900708f0b618cffb6281ce9ba3626b78aee
sompt22 <femres87@gmail.com>
2023-10-06 16:39:50 +0300

    reid extract code works, not fully tested

--- extract_feat.py
+++ extract_feat.py
@@ -5,45 +5,9 @@ from torchvision import datasets, models, transforms
 from PIL import Image
 import numpy as np
 import math
-from model import ft_net, ft_net_dense, ft_net_hr, ft_net_swin, ft_net_swinv2, ft_net_efficient, ft_net_NAS, ft_net_convnext, PCB, PCB_test
+from model import ft_net
 import torch.nn.functional as F
-
-
-parser = argparse.ArgumentParser(description='Test')
-parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
-
-
-opt = parser.parse_args()
-###load config###
-# load the training config
-
-config_path = os.path.join('./model','ft_ResNet50_reid128_noaug_bs32','opts.yaml')
-with open(config_path, 'r') as stream:
-        config = yaml.load(stream, Loader=yaml.FullLoader) # for the new pyyaml via 'conda install pyyaml'
-
-
-opt.nclasses = 751
-
-str_ids = opt.gpu_ids.split(',')
-
-gpu_ids = []
-for str_id in str_ids:
-    id = int(str_id)
-    if id >=0:
-        gpu_ids.append(id)
-
-# set gpu ids
-if len(gpu_ids)>0:
-    torch.cuda.set_device(gpu_ids[0])
-    cudnn.benchmark = True
-    
-data_dir = test_dir   
-
-# Load your pre-trained model
-model_path = '/home/fatih/phd/Person_reID_baseline_pytorch/model/ft_ResNet50_reid128_noaug_bs32/net_last.pth'
-model = ft_net(linear_num=128)
-model.load_state_dict(torch.load(model_path))  # Load your pre-trained weights
-model.eval()
+import yaml
 
 # Define a transform to preprocess images
 transform = transforms.Compose([
@@ -53,46 +17,70 @@ transform = transforms.Compose([
 ])
 
 # Define a function to extract features from a bounding box patch
-def extract_features(image_path, bbox):
-    image = Image.open(image_path).convert('RGB')
-    image = transform(image)
-    
+def extract_features(image_path, bbox, model):
     # Extract bounding box coordinates [x, y, width, height]
     x, y, w, h = bbox
-    
+    x = int(math.floor(x))
+    y = int(math.floor(y))
+    w = int(math.floor(w))
+    h = int(math.floor(h))
+    #print(x, y, w, h)
+
+    image = Image.open(image_path).convert('RGB')
+    image = image.crop((x, y, x + w, y + h))  # Crop the bounding box from the image
+    patch = transform(image)
+
+    # Move the patch and model to the GPU
+    patch = patch.cuda()
+    model = model.cuda()
+
     # Crop the bounding box patch from the image
-    patch = image[:, y:y+h, x:x+w].unsqueeze(0)  # Add a batch dimension
-    
+    patch = patch.unsqueeze(0)  # Add a batch dimension
+
     with torch.no_grad():
         features = model.extract_features(patch)
-    
+
     features = F.normalize(features, dim=1)
     # Extract the 128-dimensional feature vector
-    feature_vector = features.squeeze().numpy()
-    
+    feature_vector = features.squeeze().cpu().numpy()  # Move the feature vector back to CPU
+    #print(feature_vector.shape)
+    #print(feature_vector)
+
     return feature_vector
 
-# Load your dataset and annotation file
-data_dir = '/home/fatih/phd/mot_dataset/SOMPT22/images/train/'
-annotation_file = '/home/fatih/phd/mot_dataset/SOMPT22/annotations/train.json'
-
-# Read the annotation file
-with open(annotation_file, 'r') as f:
-    annotations = json.load(f)
-
-# Update annotations with feature vectors for each bounding box
-for annotation in annotations['annotations']:
-    image_id = annotation['image_id']
-    file_name = os.path.join(data_dir, annotations['images'][image_id - 1]['file_name'])  # Find the corresponding image file
-    bbox = annotation['bbox']  # Extract bounding box coordinates [x, y, width, height]
-    features = extract_features(file_name, bbox)
+def main():
+    # Load your pre-trained model
+    model_path = '/home/fatih/phd/Person_reID_baseline_pytorch/model/ft_ResNet50_reid128_noaug_bs32/net_last.pth'
+    model = ft_net(linear_num=128)
+    model.load_state_dict(torch.load(model_path))  # Load your pre-trained weights
+    model.eval()
     
-    # Add the feature vector to the annotation under the "embedding" key
-    annotation['embedding'] = features.tolist()
+    print(model)
 
-# Save the updated annotations back to the JSON file
-updated_annotation_file = '/home/fatih/phd/mot_dataset/SOMPT22/annotations/updated_training.json'
-with open(updated_annotation_file, 'w') as f:
-    json.dump(annotations, f)
+    # Load your dataset and annotation file
+    data_dir = '/home/fatih/phd/mot_dataset/SOMPT22/images/train/'
+    annotation_file = '/home/fatih/phd/mot_dataset/SOMPT22/annotations/train.json'
 
-# Now, 'updated_training.json' includes the 'embedding' key with feature vectors for each bounding box
+    # Read the annotation file
+    with open(annotation_file, 'r') as f:
+        annotations = json.load(f)
+
+    # Update annotations with feature vectors for each bounding box
+    for annotation in annotations['annotations']:
+        image_id = annotation['image_id']
+        file_name = os.path.join(data_dir, annotations['images'][image_id - 1]['file_name'])  # Find the corresponding image file
+        bbox = annotation['bbox']  # Extract bounding box coordinates [x, y, width, height]
+        features = extract_features(file_name, bbox, model)
+
+        # Add the feature vector to the annotation under the "embedding" key
+        annotation['embedding'] = features.tolist()
+
+    # Save the updated annotations back to the JSON file
+    updated_annotation_file = '/home/fatih/phd/mot_dataset/SOMPT22/annotations/updated_training.json'
+    with open(updated_annotation_file, 'w') as f:
+        json.dump(annotations, f)
+
+    # Now, 'updated_training.json' includes the 'embedding' key with feature vectors for each bounding box
+
+if __name__ == "__main__":
+    main()
